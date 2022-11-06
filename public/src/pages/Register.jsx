a34345d3ef25c6e5ebd49cd4c65a304f81992cda
samuelmbabhazi <samuelmbabhazi42@gmail.com>
2022-11-06 22:05:24 +0200

    implementation de de register

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,9 +1,124 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const Register = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert("form");
+  };
+  const handleChange = (event) => {};
   return (
-    <div>Register</div>
-  )
-}
+    <>
+      <FormContainer>
+        <form onSubmit={(event) => handleSubmit(event)}>
+          <div className="brand">
+            <img src="" alt="" />
+            <h1>GoChat</h1>
+          </div>
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmpassword"
+            onChange={(e) => handleChange(e)}
+          />
+          <button type="submit">Create User</button>
+          <span>
+            Already have an account ? <Link to={"/login"}>Login</Link>
+          </span>
+        </form>
+      </FormContainer>
+    </>
+  );
+};
 
-export default Register
+const FormContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: white;
+  .brand {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    justify-content: center;
+    img {
+      height: 5rem;
+    }
+    h1 {
+      color: blue;
+      text-transform: uppercase;
+    }
+  }
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    border: 0.1rem solid gray;
+    border-radius: 1rem;
+    padding: 3rem 5rem;
+    input {
+      background-color: transparent;
+      padding: 1rem;
+      border: 0.1rem solid #4e0eff;
+      border-radius: 0.4rem;
+      color: gray;
+      width: 100%;
+      font-size: 0.7rem;
+      &:focus {
+        border: 0.1rem solid blueviolet;
+        outline: none;
+      }
+    }
+    button {
+      background-color: blue;
+      color: white;
+      padding: 1rem 2rem;
+      border: none;
+      font-weight: bold;
+      cursor: pointer;
+      border-radius: 0.4rem;
+      font-size: 1rem;
+      text-transform: uppercase;
+      transition: 0.5s ease-in-out;
+      &:hover {
+        border: 0.1rem solid blue;
+        background-color: transparent;
+        color: blue;
+      }
+    }
+    span {
+      color: gray;
+      text-transform: uppercase;
+      a {
+        color: #4e0eff;
+        text-decoration: none;
+        font-weight: bold;
+      }
+    }
+  }
+`;
+
+export default Register;
