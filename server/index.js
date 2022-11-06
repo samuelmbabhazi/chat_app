607e9ce4bf939e7af523f6c47c7713d1915dabfa
samuelmbabhazi <samuelmbabhazi42@gmail.com>
2022-11-06 19:47:34 +0200

    first commit

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+require("dotenv").config();
+app.use(cors());
+app.use(express.json());
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`server actif au Port ${process.env.PORT}`);
+});
