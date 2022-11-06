d1cb4f2d9fa07211077d18c61ef3150bcfb843d1
samuelmbabhazi <samuelmbabhazi42@gmail.com>
2022-11-06 20:40:33 +0200

    connexion a la base

--- server/index.js
+++ server/index.js
@@ -7,6 +7,12 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
+mongoose.connect(process.env.MONGO_URL,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 const server = app.listen(process.env.PORT, () => {
   console.log(`server actif au Port ${process.env.PORT}`);
 });
