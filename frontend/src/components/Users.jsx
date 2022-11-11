60a0dc0bbc71b9349c9cd2e05f77ed7e594ebd56
samuelmbabhazi <samuelmbabhazi42@gmail.com>
2022-11-11 19:54:20 +0200

    ajout de contact

---
+++ frontend/src/components/Users.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Users = ({ users }) => {
+  return (
+    <div
+      id="contact"
+      class="flex flex-col space-y-1 mt-4 -mx-2 h-[100%] overflow-y-auto "
+    >
+      {users &&
+        users.map((name, index) => {
+          return (
+            <button class="flex flex-row items-center hover:border-b-2 hover:border-[blue]  p-2">
+              <div class="flex items-center justify-center h-8 w-8 bg-[blue] rounded-full">
+                {name.username[0].toUpperCase()}
+              </div>
+              <div class="ml-2 text-sm font-semibold">{name.username}</div>
+              <div class="flex items-center justify-center ml-auto text-xs text-white bg-red-500 h-5 w-5 rounded-full leading-none">
+                3
+              </div>
+            </button>
+          );
+        })}
+    </div>
+  );
+};
+
+export default Users;
--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import { usersRouter } from "../utils/Api";
 import Message from "../components/Message";
 import Input from "../components/Input";
+import Users from "../components/Users";
 
 const Chat = () => {
   const [users, setUsers] = useState([]);
@@ -57,27 +58,7 @@ const Chat = () => {
                   </button>{" "}
                 </span>
               </div>
-              <div
-                id="contact"
-                class="flex flex-col space-y-1 mt-4 -mx-2 h-[100%] overflow-y-auto "
-              >
-                {users &&
-                  users.map((name, index) => {
-                    return (
-                      <button class="flex flex-row items-center hover:border-b-2 hover:border-[blue]  p-2">
-                        <div class="flex items-center justify-center h-8 w-8 bg-[blue] rounded-full">
-                          {name.username[0].toUpperCase()}
-                        </div>
-                        <div class="ml-2 text-sm font-semibold">
-                          {name.username}
-                        </div>
-                        <div class="flex items-center justify-center ml-auto text-xs text-white bg-red-500 h-5 w-5 rounded-full leading-none">
-                          2
-                        </div>
-                      </button>
-                    );
-                  })}
-              </div>
+              <Users users={users} />
             </div>
           </div>
           <Message />
