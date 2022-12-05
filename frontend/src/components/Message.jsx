cc5a49513fbb8ef1a4dd8a4b9113887bf204eb94
samuelmbabhazi <samuelmbabhazi42@gmail.com>
2022-12-05 14:39:00 +0200

    add readme

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -4,7 +4,7 @@ const Message = ({ messages, toId, currentId, formatDateFromTimestamp }) => {
   return (
     <>
       <div className="flex flex-col h-full">
-        <div className="grid grid-cols-12 gap-y-2">
+        <div className="grid grid-cols-12 gap-y-2 ">
           {messages &&
             messages.map((mes, key) => {
               if (mes.to === toId && mes.from === currentId) {
@@ -26,7 +26,7 @@ const Message = ({ messages, toId, currentId, formatDateFromTimestamp }) => {
               } else if (mes.to === currentId && mes.from === toId) {
                 const mymes = mes.message;
                 return (
-                  <div key={key} className="col-start-1 col-end-12  rounded-lg">
+                  <div key={key} className="col-start-2 col-end-12  rounded-lg">
                     <div className="flex flex-row items-center">
                       <div className="relative flex  gap-2 text-sm bg-white py-2 px-2 shadow rounded-xl text-black">
                         <div>{mymes}</div>
